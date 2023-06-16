2e29d6a0d5ffbea642502a869ca5a4c868579004
Oğuzhan Yılmaz <ibrahimoguzhany@gmail.com>
2023-06-16 09:19:45 +0300

    AppNavbar added, App language updated.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import { APP_ROUTES } from './utils/constants';
-import Calendar from './components/Calendar';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import ExampleNavbar from "./components/AppNavbar";
+import Dashboard from "./components/Dashboard";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import { APP_ROUTES } from "./utils/constants";
+import Calendar from "./components/Calendar";
 
 export default function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Navigate to={APP_ROUTES.DASHBOARD} />} />
-          <Route path={APP_ROUTES.SIGN_UP} exact element={<SignUp />} />
-          <Route path={APP_ROUTES.SIGN_IN} element={<SignIn />} />
-          <Route path={APP_ROUTES.DASHBOARD} element={<Dashboard />} />
-          <Route path={APP_ROUTES.CALENDAR} element={<Calendar />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+  return (
+    <BrowserRouter>
+      <ExampleNavbar />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Navigate to={APP_ROUTES.DASHBOARD} />}
+        />
+        <Route path={APP_ROUTES.SIGN_UP} exact element={<SignUp />} />
+        <Route path={APP_ROUTES.SIGN_IN} element={<SignIn />} />
+        <Route path={APP_ROUTES.DASHBOARD} element={<Dashboard />} />
+        <Route path={APP_ROUTES.CALENDAR} element={<Calendar />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
